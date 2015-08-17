81511594ae442075ed16b4dd023fa50e3d6ba8d5
Bardia Keyoumarsi <b.keyoumarsi@gmail.com>
2015-08-17 15:13:40 -0700

    Added update directory acl api

--- api/namespace.go
+++ api/namespace.go
@@ -1,18 +1,13 @@
 package goisilon
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 
+	types "github.com/bkeyoumarsi/goisilon/types/v1"
 	"github.com/mitchellh/mapstructure"
 )
-
-type DirChildren struct {
-	Name string `json:"name"`
-}
-
-type Directory struct {
-	Children []DirChildren `json:"children"`
-}
 
 // Path arguemnt must start with /ifs/
 func (c *IsiClient) CreateDirectory(path string, headers map[string]string, recursive bool) error {
@@ -32,7 +27,7 @@ func (c *IsiClient) CreateDirectory(path string, headers map[string]string, recu
 	return err
 }
 
-// Path arguemnt must start with /ifs/
+// parentDir arguemnt must start with /ifs/
 func (c *IsiClient) DirectoryExists(parentDir, dirName string) (bool, error) {
 	url := fmt.Sprintf("/namespace/%s", parentDir)
 
@@ -41,7 +36,7 @@ func (c *IsiClient) DirectoryExists(parentDir, dirName string) (bool, error) {
 		return false, err
 	}
 
-	var content Directory
+	var content types.Directory
 	err = mapstructure.Decode(data, &content)
 	if err != nil {
 		return false, err
@@ -64,5 +59,17 @@ func (c *IsiClient) DeleteDirectory(path string, recursive bool) error {
 	url := fmt.Sprintf("/namespace/%s?recursive=%s", path, rec)
 
 	_, err := c.HttpClient.Do("DELETE", url, nil, nil)
+	return err
+}
+
+func (c *IsiClient) UpdateDirAcl(path string, body types.AclRequest) error {
+	url := fmt.Sprintf("/namespace/%s?acl", path)
+
+	b, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.HttpClient.Do("PUT", url, nil, bytes.NewBuffer(b))
 	return err
 }
--- api/namespace_test.go
+++ api/namespace_test.go
@@ -1,6 +1,10 @@
 package goisilon
 
-import "testing"
+import (
+	"testing"
+
+	types "github.com/bkeyoumarsi/goisilon/types/v1"
+)
 
 const (
 	endpoint = "https://10.28.102.200:8080"
@@ -24,6 +28,20 @@ func TestDirectoryExists(t *testing.T) {
 	c := NewApiClient(endpoint, username, password, insecure)
 	exists, err := c.DirectoryExists("/ifs/data/", "temp_dir")
 	if err != nil || !exists {
+		t.Fail()
+	}
+}
+
+func TestUpdateAcl(t *testing.T) {
+	c := NewApiClient(endpoint, username, password, insecure)
+	var req = types.AclRequest{
+		Authoritative: "acl",
+		Action:        "update",
+		Owner:         types.Ownership{Id: "UID:65534", Name: "nobody", Type: "user"},
+		Group:         types.Ownership{Id: "GID:65534", Name: "nobody", Type: "group"},
+	}
+	err := c.UpdateDirAcl("/ifs/data/temp_dir", req)
+	if err != nil {
 		t.Fail()
 	}
 }
